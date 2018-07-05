51093a2487061306ba0d209825c8c6f40fff2bd6
hlxia <1104222436@qq.com>
2018-07-06 01:54:51 +0800

    add RestaurantService.java

---
+++ restaurant-service/src/main/java/com/it/xiapsc/domain/service/RestaurantService.java
@@ -0,0 +1,7 @@
+package com.it.xiapsc.domain.service;
+
+import com.it.xiapsc.base.service.BaseService;
+import com.it.xiapsc.domain.mode.Restaurant;
+
+public class RestaurantService implements BaseService<Restaurant, String> {
+}
